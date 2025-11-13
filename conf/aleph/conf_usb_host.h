66ec7de52e7a3912e9f6a34bdf05f2fab44ae8e5
BTrailor <brett.gershon@gmail.com>
2025-11-13 11:43:14 -0500

    Fix USB driver order: FTDI, CDC, HID (was HID first), add CDC driver

--- conf/aleph/conf_usb_host.h
+++ conf/aleph/conf_usb_host.h
@@ -13,7 +13,7 @@
 
 //! Array of UHI APIs Define the list of UHI supported by USB host.
 //#define USB_HOST_UHI        UHI_HID_MOUSE , UHI_HID_GAMEPAD , UHI_FTDI , UHI_MIDI ,
-#define USB_HOST_UHI        UHI_HID , UHI_FTDI , UHI_MIDI ,
+#define USB_HOST_UHI        UHI_FTDI , UHI_CDC , UHI_HID , UHI_MIDI
 
 //! Maximum current allowed on Vbus (mA)
 #define USB_HOST_POWER_MAX  500
@@ -74,6 +74,7 @@
 
 
 #include "uhi_ftdi.h"
+#include "uhi_cdc.h"
 #include "uhi_hid.h"
 #include "uhi_midi.h"
 //#include "ui.h"
