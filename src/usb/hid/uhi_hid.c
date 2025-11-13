2421eb7b8c651ec10348698b41050d66dc0d610c
BTrailor <brett.gershon@gmail.com>
2025-11-12 23:45:11 -0500

    HID driver: reject monome VIDs to allow CDC driver to handle grids

--- src/usb/hid/uhi_hid.c
+++ src/usb/hid/uhi_hid.c
@@ -83,9 +83,19 @@ uhc_enum_status_t uhi_hid_install(uhc_device_t* dev) {
   bool b_iface_supported;
   uint16_t conf_desc_lgt;
   usb_iface_desc_t *ptr_iface;
+  uint16_t vid;
+
+  print_dbg("\r\n HID: uhi_hid_install() called");
 
   if (uhi_hid_dev.dev != NULL) {
     return UHC_ENUM_SOFTWARE_LIMIT; // Device already allocated
+  }
+
+  // Reject monome devices - they should be handled by CDC driver
+  vid = le16_to_cpu(dev->dev_desc.idVendor);
+  if(vid == 0x16c0 || vid == 0x0483) {
+    print_dbg("\r\n HID: rejecting monome VID, deferring to CDC driver");
+    return UHC_ENUM_UNSUPPORTED;
   }
   conf_desc_lgt = le16_to_cpu(dev->conf_desc->wTotalLength);
   ptr_iface = (usb_iface_desc_t*)dev->conf_desc;
